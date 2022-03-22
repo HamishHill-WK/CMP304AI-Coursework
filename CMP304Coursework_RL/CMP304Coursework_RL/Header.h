f4d11bb3ed5047c9201698a1e67e41cbe0e33e8f
HamishHill-WK <57226991+HamishHill-WK@users.noreply.github.com>
2022-03-22 12:21:20 +0000

    Done loads of work

--- CMP304Coursework_RL/CMP304Coursework_RL/Header.h
+++ CMP304Coursework_RL/CMP304Coursework_RL/Header.h
@@ -13,27 +13,31 @@
 #define ITEMS_VISIBLE 3
 
 enum tag { NoTag = 0, Apparel, Computing, Jewellery, Gardening};
+static const char* EnumStrings[] = { "NoTag" , "Apparel", "Computing", "Jewellery", "Gardening"};
 
-	struct item
-	{
-		std::string name = "itemName";
-		tag tags = NoTag;
-	};
+struct item
+{
+	std::string name = "itemName";
+	tag tags = NoTag;
+};
 
 class application
 {
-
-
 public:
 	application();
 	~application();
 
-	item item1[TOTAL_ITEM_NUM];
+	std::vector<item> item1;
+	std::vector<std::vector<int>> combinationCodes;
+	std::vector<int> combination;
+	std::vector<int> itemID;
 	int numOfStates = 0;
 
 	void setUpItems();
 	void calcNumOfStates();
 	int calcFactorial(int i);
+	void createCombinations(int offset, int k);
+	const char* getTextForEnum(int enumVal);
 
 private:
 	
@@ -41,17 +45,7 @@ private:
 
 application::application()
 {
-	//item1[0].name = "Shirt";
-	//item1[0].tags = Apparel;
 
-	//item1[1].name = "Keyboard";
-	//item1[1].tags = Computing;
-
-	//item1[2].name = "Ring";
-	//item1[2].tags = Jewellery;
-
-	//item1[3].name = "Shovel";
-	//item1[3].tags = Gardening;
 }
 
 application::~application()
@@ -60,46 +54,95 @@ application::~application()
 
 inline void application::setUpItems()
 {
+	for (int i = 0; i < TOTAL_ITEM_NUM; ++i)
+		itemID.push_back(i );
+
+	item1.resize(TOTAL_ITEM_NUM);
+
 	if (TOTAL_ITEM_NUM == 4)
 	{
-		item1[0].name = "Shirt";
-		item1[0].tags = Apparel;
+		item1.at(0).name = "Shirt";
+		item1.at(0).tags = Apparel;
 
-		item1[1].name = "Keyboard";
-		item1[1].tags = Computing;
+		item1.at(1).name = "Keyboard";
+		item1.at(1).tags = Computing;
 
-		item1[2].name = "Ring";
-		item1[2].tags = Jewellery;
+		item1.at(2).name = "Ring";
+		item1.at(2).tags = Jewellery;
 
-		item1[3].name = "Shovel";
-		item1[3].tags = Gardening;
+		item1.at(3).name = "Shovel";
+		item1.at(3).tags = Gardening;
 	}
 
 	if (TOTAL_ITEM_NUM == 8)
 	{
-		item1[0].name = "Shirt";
-		item1[0].tags = Apparel;
+		//apparel
+		item1.at(0).name = "Shirt";
+		item1.at(0).tags = Apparel;
 
-		item1[1].name = "Jeans";
-		item1[1].tags = Apparel;
+		item1.at(1).name = "Jeans";
+		item1.at(1).tags = Apparel;
 
-		item1[2].name = "Keyboard";
-		item1[2].tags = Computing;
+		//computing
+		item1.at(2).name = "Keyboard";
+		item1.at(2).tags = Computing;
 
-		item1[3].name = "Mouse";
-		item1[3].tags = Computing;
+		item1.at(3).name = "Mouse";
+		item1.at(3).tags = Computing;
 
-		item1[4].name = "Ring";
-		item1[4].tags = Jewellery;
+		//jewellery
+		item1.at(4).name = "Ring";
+		item1.at(4).tags = Jewellery;
 
-		item1[5].name = "Earrings";
-		item1[5].tags = Jewellery;
+		item1.at(5).name = "Earrings";
+		item1.at(5).tags = Jewellery;
 
-		item1[6].name = "Shovel";
-		item1[6].tags = Gardening;
+		//gardening
+		item1.at(6).name = "Shovel";
+		item1.at(6).tags = Gardening;
 
-		item1[7].name = "Garden Seeds";
-		item1[7].tags = Gardening;
+		item1.at(7).name = "Garden Seeds";
+		item1.at(7).tags = Gardening;
+	}	
+	
+	if (TOTAL_ITEM_NUM == 12)
+	{
+		//apparel
+		item1.at(0).name = "Shirt";
+		item1.at(0).tags = Apparel;
+
+		item1.at(1).name = "Jeans";
+		item1.at(1).tags = Apparel;		
+		
+		item1.at(2).name = "Shoes";
+		item1.at(2).tags = Apparel;
+
+		item1.at(3).name = "Keyboard";
+		item1.at(3).tags = Computing;
+
+		item1.at(4).name = "Mouse";
+		item1.at(4).tags = Computing;
+
+		item1.at(5).name = "RAM sticks";
+		item1.at(5).tags = Computing;
+
+		item1.at(6).name = "Earrings";
+		item1.at(6).tags = Jewellery;
+
+		item1.at(7).name = "Ring";
+		item1.at(7).tags = Jewellery;
+
+		item1.at(8).name = "Necklace";
+		item1.at(8).tags = Jewellery;
+
+		item1.at(9).name = "Shovel";
+		item1.at(9).tags = Gardening;
+
+		item1.at(10).name = "Garden Seeds";
+		item1.at(10).tags = Gardening;
+
+		item1.at(11).name = "Hoe";
+		item1.at(11).tags = Gardening;
 	}
 }
 
@@ -109,9 +152,27 @@ inline int application::calcFactorial(int i)
 	for (int j = 1; j < i; j++)
 		factNum *= j;
 
-	std::cout << "factorial: " << factNum << "\n";
+	//std::cout << "factorial: " << factNum << "\n";
 
 	return factNum;
+}
+
+inline void application::createCombinations(int offset, int i)
+{
+	if (i == 0) {
+		combinationCodes.push_back(combination);
+		return;
+	}
+	for (int j = offset; j <= TOTAL_ITEM_NUM- i; ++j) {
+		combination.push_back(itemID[j]);
+		createCombinations(j + 1, i - 1);
+		combination.pop_back();
+	}
+}
+
+inline const char* application::getTextForEnum(int enumVal)
+{
+	return EnumStrings[enumVal];
 }
 
 inline void application::calcNumOfStates()
--- CMP304Coursework_RL/CMP304Coursework_RL/Source.cpp
+++ CMP304Coursework_RL/CMP304Coursework_RL/Source.cpp
@@ -1,81 +1,53 @@
 #include "Header.h"
 using namespace std::chrono_literals;
 
-enum State { STABLE = 0, TOO_MUCH_PRESSURE, TOO_LITTLE_PRESSURE };
-enum Action { WAIT = 0, ADD_WATER, RELEASE_WATER };
-
-//std::array<std::array<float, ACTION_NUM>, STATE_NUM> Q = { {0} };
-//std::array<std::string, ACTION_NUM> action_to_string = { "WAIT", "ADD WATER", "RELEASE WATER" };
-//std::array<std::string, ACTION_NUM> state_to_string = { "STABLE", "TOO MUCH PRESSURE", "TOO LITTLE PRESSURE" };
-
 application app;
 
-std::array<item, TOTAL_ITEM_NUM> array_of_items;
-std::array<item, ITEMS_VISIBLE> array_of_items_visible;
-
-void printMatrix()
-{
-	using namespace std;
-	cout << "Q Matrix" << endl << endl;
-	for (int i = 0; i < STATE_NUM; i++)
-	{
-		for (int j = 0; j < ACTION_NUM; j++)
-		{
-			//cout << "For state \"" << state_to_string[i] << "\" the action \"" << action_to_string[j] << "\" has a value of: " << Q[i][j] << endl;
-		}
-	}
+void pretty_print(const std::vector<float>& v) {
+	static int count = 0;
+	std::cout << "combination no " << (++count) << ": [ ";
+	for (int i = 0; i < v.size(); ++i) { std::cout << v[i] << " "; }
+	std::cout << "] " << std::endl;
 }
 
-int action(State currentS)
-{
-	float bestV = Q[currentS][0];
-
-	int random = 0;
-
-	int action = 0;
-
-	random = rand() % 100;
-
-	for (int i = 0; i < ACTION_NUM; i++)
-	{
-		if (random >= 10)
-			if (Q[currentS][i] > bestV)
-			{
-				std::cout << " ------------ " << std::endl;
-				std::cout << " ?? NOT RANDOM ?? " << std::endl;
-				std::cout << " ------------ " << std::endl;
-
-				bestV = Q[currentS][i];
-				action = i;
-			}
-
-		if (random < 10)
-		{
-			std::cout << " ------------ " << std::endl;
-			std::cout << " @@ RANDOM @@ " << std::endl;
-			std::cout << " ------------ " << std::endl;
-
-			if (random <= 2)
-				i = 0;
-
-			if (random >= 7)
-				i = 1;
-
-			else
-				i = 2;
-
-			bestV = Q[currentS][i];
-			action = i;
-			break;
-		}
-	}
-
-	std::cout << "bestV: " << bestV << std::endl;
-
-	return action;
+void pretty_print1(const std::vector<int>& v) {
+	static int count = 0;
+	std::cout << "combination no " << (++count) << ": [ ";
+	for (int i = 0; i < v.size(); ++i) { std::cout << v[i] << " "; }
+	std::cout << "] " << std::endl;
 }
 
+float calcReward(tag interest1, item chosenItem, int satisfaction)
+{
+	if (satisfaction >= 3)
+	{
+		if (interest1 == chosenItem.tags)
+			return 5.0f;
 
+		if (interest1 != chosenItem.tags)
+			return 1.0f;
+	}
+
+	if (satisfaction == 1)
+	{
+		if (interest1 == chosenItem.tags)
+			return -1.0f;
+
+		if (interest1 != chosenItem.tags)
+			return -5.0f;
+	}
+
+
+	if (satisfaction < 3)
+	{
+		if (interest1 == chosenItem.tags)
+			return 2.0f;
+
+		if (interest1 != chosenItem.tags)
+			return 0.0f;
+	}
+
+}
 
 int main()
 {
@@ -83,140 +55,139 @@ int main()
 
 	app.setUpItems();
 	app.calcNumOfStates();
+	app.createCombinations(0, ITEMS_VISIBLE);
+	tag interest = NoTag;
 
 	int states = 0;
 	states = app.numOfStates;
-	item itemX, itemY, itemZ;
 
-	//app.item1[0], app.item1[1], app.item1[2]
 	std::vector<std::vector<float>> Q;
 	Q.resize(states);
 	for (int i = 0; i < states; i++)
 		Q[i].resize(ITEMS_VISIBLE);
 
-	float s = Q[0][0];
+	bool running = true;
+	int input = 0;
 
-	std::vector<std::vector<item>> itemStates;// = { itemX, itemY, itemZ };
-	itemStates.resize(states);
+	std::cout << "please enter your main interest from: Apparel(1), Computing(2), Jewellery(3), Gardening(4)" << std::endl;
+	std::cin >> input;
+
+	if (input == 1)
+		interest = Apparel;	
+	
+	if (input == 2)
+		interest = Computing;	
+	
+	if (input == 3)
+		interest = Jewellery;	
+	
+	if (input == 4)
+		interest = Gardening;
+
+	for (int i = 0; i < states; i++)	//add bias towards the user's prefferred interest at start 
+	{
+		if(app.item1.at(app.combinationCodes.at(i).at(0)).tags == interest)
+			Q.at(i).at(0) += 1.0f;
+
+		if (app.item1.at(app.combinationCodes.at(i).at(1)).tags == interest)
+			Q.at(i).at(1) += 1.0f;
+
+		if (app.item1.at(app.combinationCodes.at(i).at(2)).tags == interest)
+			Q.at(i).at(2) += 1.0f;
+	}
 
 	for (int i = 0; i < states; i++)
-	{
-		for (int j = 0; j < TOTAL_ITEM_NUM; j++)
-		{
-			if (j < 7)
-			{
-				itemX = app.item1[j];
-				itemY = app.item1[j + 1];
-				itemZ = app.item1[j + 2];
+		pretty_print(Q.at(i));
 
+	while (running)
+	{
+		int x = rand() % (TOTAL_ITEM_NUM * ITEMS_VISIBLE);
+		std::cout << app.item1.at(app.combinationCodes.at(x).at(0)).name << " " << app.item1.at(app.combinationCodes.at(x).at(1)).name << " " << app.item1.at(app.combinationCodes.at(x).at(2)).name << "\n";
+
+		input = 0;
+		int satisfaction = 0;
+		float reward = 0;
+		std::cin >> input;
+
+		if (input == 1)
+		{
+			std::cout << "are you happy with this item ?: " << app.item1.at(app.combinationCodes.at(x).at(0)).name
+				<< " tags: " << app.getTextForEnum(app.item1.at(app.combinationCodes.at(x).at(0)).tags) << "\n";
+
+			std::cin >> satisfaction;
+			reward = calcReward(interest, app.item1.at(app.combinationCodes.at(x).at(0)), satisfaction);
+			//Q.at(x).at(0) = reward; 
+			for (int i = 0; i < states; i++)	//update weighting for each instance of this item in vector of items
+			{
+				if (app.combinationCodes.at(i).at(0) == app.combinationCodes.at(x).at(0))
+					Q.at(i).at(0) += reward;
+
+				if (app.combinationCodes.at(i).at(0) == app.combinationCodes.at(x).at(1))
+					Q.at(i).at(1) += reward;
+
+				if (app.combinationCodes.at(i).at(0) == app.combinationCodes.at(x).at(2))
+					Q.at(i).at(2) += reward;
 			}
-			itemStates.at(i+j) = { itemX, itemY, itemZ };
+		}
+
+		if (input == 2)
+		{
+			std::cout << "are you happy with this item ?: " << app.item1.at(app.combinationCodes.at(x).at(1)).name
+				<< " tags: " << app.item1.at(app.combinationCodes.at(x).at(1)).tags << "\n";
+
+			std::cin >> satisfaction;
+			reward = calcReward(interest, app.item1.at(app.combinationCodes.at(x).at(1)), satisfaction);
+
+			for (int i = 0; i < states; i++)
+			{
+				if (app.combinationCodes.at(i).at(1) == app.combinationCodes.at(x).at(0))
+					Q.at(i).at(0) += reward;
+
+				if (app.combinationCodes.at(i).at(1) == app.combinationCodes.at(x).at(1))
+					Q.at(i).at(1) += reward;
+
+				if (app.combinationCodes.at(i).at(1) == app.combinationCodes.at(x).at(2))
+					Q.at(i).at(2) += reward;
+			}
 		}
 		
-	}
-	 //state XYZ
-
-	float system_pressure = 50.f;
-
-	float learningRate = .1f;
-
-	float discountFactor = 0.25f;
-
-	float maxQ = 10.0f;
-
-	int iterator = 0;
-
-	int actionInt = 0;
-
-	float reward = 0;
-
-	float LM[3][3] = { {0} };
-
-
-	Action chosen_action = WAIT;
-
-	while (system_pressure > 0)
-	{
-		//std::cout << " ------------ " << std::endl;
-		//std::cout << "System pressure is sitting at: " << system_pressure << "." << std::endl;
-		//std::cout << " ------------ " << std::endl;
-
-		// Describe State
-		// TODO: Store a local variable that describes the current state
-		//currentState = currentPressure(system_pressure);
-
-		//actionInt = action(currentState);
-
-		// Choose Action
-		// TODO: Store a local variable that explains what action the AI will perform
-
-		// Perform the Action
-		// TODO: Implement the chosen action. Use a switch statement or function to manage this.
-		if (actionInt == 0)
-			chosen_action = Action::WAIT;
-
-		if (actionInt == 1)
-			chosen_action = Action::ADD_WATER;
-
-		if (actionInt == 2)
-			chosen_action = Action::RELEASE_WATER;
-
-		// Calculate Reward
-		// TODO: Calculate the reward you want to give to the AI
-		//reward = calcReward(currentState, chosen_action);
-		//Q[currentState][chosen_action] = Q[currentState][chosen_action] + (learningRate * (reward + (discountFactor * maxQ) - Q[currentState][chosen_action]));
-
-		// Update Q Matrix
-		// TODO: Update the correct item the Q matrix with the correct reward.
-
-		// TODO: Print out which action the AI ran. You can use a similar method to the action_to_string or state_to_string described in the printMatrix function
-
-		switch (chosen_action)
+		if (input == 3)
 		{
-		case Action::WAIT:
-			// TODO: Add code
-			std::cout << " ------------ " << std::endl;
-			std::cout << "--- I'm waiting ---" << std::endl;
-			std::cout << " ------------ " << std::endl;
-			break;
-		case Action::ADD_WATER:
-			// TODO: Add code
-			std::cout << " ------------ " << std::endl;
-			std::cout << "### adding water ###" << std::endl;
-			std::cout << " ------------ " << std::endl;
-			system_pressure += 10.0f;
-			break;
-		case Action::RELEASE_WATER:
-			// TODO: Add code
-			std::cout << " ------------ " << std::endl;
-			std::cout << "%%% releasing water %%%" << std::endl;
-			std::cout << " ------------ " << std::endl;
-			system_pressure -= 10.0f;
-			break;
-		default:
-			// We should never reach this point
-			// but its good to prepare for it just in case
-			std::cout << "_ I'm stuck in default state _" << std::endl;
-			break;
+			//std::string tag = app.item1.at(app.combinationCodes.at(x).at(2)).tags;
+			std::cout << "are you happy with this item? Please rate from 1 to 5. Item: " << app.item1.at(app.combinationCodes.at(x).at(2)).name
+				<< " tags: " << app.item1.at(app.combinationCodes.at(x).at(2)).tags << "\n";
+
+			std::cin >> satisfaction;
+			reward = calcReward(interest, app.item1.at(app.combinationCodes.at(x).at(2)), satisfaction);
+
+			for (int i = 0; i < states; i++)
+			{
+				if (app.combinationCodes.at(i).at(2) == app.combinationCodes.at(x).at(0))
+					Q.at(i).at(0) += reward;
+
+				if (app.combinationCodes.at(i).at(2) == app.combinationCodes.at(x).at(1))
+					Q.at(i).at(1) += reward;
+
+				if (app.combinationCodes.at(i).at(2) == app.combinationCodes.at(x).at(2))
+					Q.at(i).at(2) += reward;
+			}
 		}
 
+		if (input == 4)
+			running = false;
 
-
-		system_pressure -= (rand() % 4);
-		std::cout << " ++++++++++++++ " << std::endl;
-		std::cout << "System has lost some pressure." << std::endl;
-		std::cout << " ++++++++++++++ " << std::endl;
-		std::this_thread::sleep_for(100ms);
-		iterator++;
-
-		if (iterator == 50)
-			break;
-
-		//learningRate -= .1f;
-		//discountFactor -= .1f;
 	}
 
-	printMatrix();
+	for(int i = 0; i < states; i++)
+	pretty_print(Q.at(i));	
+	
+	for(int i = 0; i < states; i++)
+	pretty_print1(app.combinationCodes.at(i));
+
+	// Calculate Reward
+	// TODO: Calculate the reward you want to give to the AI
+	//reward = calcReward(currentState, chosen_action);
+	//Q[currentState][chosen_action] = Q[currentState][chosen_action] + (learningRate * (reward + (discountFactor * maxQ) - Q[currentState][chosen_action]));
 
 	std::cin.get();
 }
